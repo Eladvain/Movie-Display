cb51be4b4f4aa95adf4f22eef3bfffb4f536488f
Eladvain <eladvainberg@gmail.com>
2024-08-11 14:43:22 +0300

    add bookmarked button

--- front-app/src/components/MovieCard.js
+++ front-app/src/components/MovieCard.js
@@ -8,9 +8,14 @@ const MovieCard = ({movieItem}) => {
         <img id='pic' src={movieItem.urlToImage} alt='png'></img>
       </div>
       <div className='sep-card details-movie'>
-        <div className='name-div'>
-          <p id = 'movie-name'>{movieItem.source['name']}</p>
-        </div>  
+        <div className='headline-div'>
+          <div className='name-div'>
+            <h1 id = 'movie-name'>{movieItem.source['name']}</h1>
+          </div>
+          <div className='bookmarked-div'>
+            <button id = 'bookmarked-btn'>bookmarked</button>
+          </div>
+        </div>    
         <div className='title-div'>
           <p id='movie-title'>{movieItem.title}</p>
         </div>
